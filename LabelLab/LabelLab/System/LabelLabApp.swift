4a1c2411b31763b70a2a765654712401b6ee835a
HoJong <pjh00098@gmail.com>
2022-07-18 22:39:51 +0900

    Chore: Add project file

---
+++ LabelLab/LabelLab/System/LabelLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabelLabApp.swift
+//  LabelLab
+//
+//  Created by JongHo Park on 2022/07/18.
+//
+
+import SwiftUI
+
+@main
+struct LabelLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
