ca1bac565441a06be677dbd4df8e5adc4990aa40
HoJong <pjh00098@gmail.com>
2022-08-18 18:12:36 +0900

    Docs: Add Firebase Dependency

--- LabelLab/LabelLab/System/LabelLabApp.swift
+++ LabelLab/LabelLab/System/LabelLabApp.swift
@@ -5,10 +5,16 @@
 //  Created by JongHo Park on 2022/07/18.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct LabelLabApp: App {
+
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
